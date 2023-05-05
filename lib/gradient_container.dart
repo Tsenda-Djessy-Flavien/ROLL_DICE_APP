3d802a847b5f9d5a7737329829a8d2d7f7a7e228
Tsenda-Djessy-Flavien <djesstsenda@gmail.com>
2023-05-05 16:48:03 +0100

    Styliser les boutons et travailler avec le rembourrage

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -32,11 +32,17 @@ class GradientContainer extends StatelessWidget {
       ),
       child: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
           children: [
             Image.asset('assets/images/dice-2.png', width: 200),
+            const SizedBox(height: 20),
             TextButton(
               onPressed: RollDice,
+              style: TextButton.styleFrom(
+                // padding: const EdgeInsets.only(top: 20),
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(fontSize: 28),
+              ),
               child: const Text('Roll Dice'),
             ),
           ],
