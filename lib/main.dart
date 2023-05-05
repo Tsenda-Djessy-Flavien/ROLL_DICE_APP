7f65ddb61f92e8ef998676d28c20f8a729317410
Tsenda-Djessy-Flavien <djesstsenda@gmail.com>
2023-05-05 11:11:58 +0100

    Génériques, listes et ajout de dégradés de couleurs

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 75, 39, 95), // Colors.deepPurple,
-        body: Center(
-          child: Text('Hello World !'),
+        // backgroundColor: Color.fromARGB(255, 75, 39, 95), // Colors.deepPurple,
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 53, 5, 62),
+                Color.fromARGB(255, 70, 6, 81),
+              ],
+            ),
+          ),
+          child: const Center(
+            child: Text('Hello World !'),
+          ),
         ),
       ),
     ),
