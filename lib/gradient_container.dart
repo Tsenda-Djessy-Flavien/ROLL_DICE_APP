0c9ea9bc71b6a68f6b1017332724f6a4d72bf3df
Tsenda-Djessy-Flavien <djesstsenda@gmail.com>
2023-05-05 12:52:08 +0100

    Présentation des variables

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:roll_dice_app/styled_text.dart';
 
+var startAligment = Alignment.topLeft;
+var endAligment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
+          colors: const [
             Color.fromARGB(255, 26, 2, 80),
             Color.fromARGB(255, 45, 7, 98),
           ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: startAligment,
+          end: endAligment,
         ),
       ),
       child: const Center(child: StyledText('Hello World !')),
