165da499757f16a2afca6c78d5440444aa12eaf4
Tsenda-Djessy-Flavien <djesstsenda@gmail.com>
2023-05-05 12:16:34 +0100

    Fractionner le code entre les fichiers

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:roll_dice_app/gradient_container.dart';
 
 void main() {
   runApp(
@@ -9,33 +10,4 @@ void main() {
       ),
     ),
   );
-}
-
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 26, 2, 80),
-            Color.fromARGB(255, 45, 7, 98),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          'Hello World !',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 28.5,
-          ),
-        ),
-      ),
-    );
-  }
 }
