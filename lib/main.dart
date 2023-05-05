e03f827aa96ec131a330727c6256fada61ffe32c
Tsenda-Djessy-Flavien <djesstsenda@gmail.com>
2023-05-05 10:34:18 +0100

    Configuration des widgets et compréhension des objets

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 75, 39, 95), // Colors.deepPurple,
         body: Center(
           child: Text('Hello World !'),
         ),
