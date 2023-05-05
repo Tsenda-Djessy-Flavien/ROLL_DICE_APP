349ba2cdbab880c0980a1cb08285408e2f7ef092
Tsenda-Djessy-Flavien <djesstsenda@gmail.com>
2023-05-05 12:10:33 +0100

    Création de widgets personnalisés

--- lib/main.dart
+++ lib/main.dart
@@ -2,31 +2,40 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
         // backgroundColor: Color.fromARGB(255, 75, 39, 95), // Colors.deepPurple,
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 26, 2, 80),
-                Color.fromARGB(255, 45, 7, 98),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello World !',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 28.5,
-              ),
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(255, 45, 7, 98),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello World !',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28.5,
+          ),
+        ),
+      ),
+    );
+  }
 }
